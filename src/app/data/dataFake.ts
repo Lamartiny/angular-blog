ba9a1fa63d531b199baa2b699265b2b80338c33a
Lamartiny Nunes <lamartinysjpatos@gmail.com>
2023-10-14 15:31:13 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "TESTE DINÂMICO",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "photoCover": "https://img.ibxk.com.br/2023/10/11/11012528601520.jpg?ims=704x"
+    },
+    {
+        "id": "2",
+        "title": "TESTE DINÂMICO 1",
+        "description": "Teste dinâmico descrição 1",
+        "photoCover": "https://img.ibxk.com.br/2023/10/11/11192520700370.jpg?ims=704x"
+    },
+    {
+        "id": "3",
+        "title": "TESTE DINÂMICO 2",
+        "description": "Teste dinâmico descrição 2",
+        "photoCover": "https://img.ibxk.com.br/2023/10/13/13165102070146.jpg?ims=704x"
+    },
+    {
+        "id": "4",
+        "title": "TESTE DINÂMICO 3",
+        "description": "Teste dinâmico descrição 3",
+        "photoCover": "https://img.ibxk.com.br/2023/10/11/11151311806246.jpg?ims=704x"
+    },
+]
